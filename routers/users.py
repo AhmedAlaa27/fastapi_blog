fe8f0de338d1aedcebc2d286df9017f3f7305348
AhmedAlaa27 <ahmedalaa277143@gmail.com>
2026-03-12 10:48:28 +0200

    9) Enhance post management with edit and delete modals, and implement post ordering by date

--- routers/users.py
+++ routers/users.py
@@ -123,6 +123,10 @@ async def get_user_posts(user_id: int, db: Annotated[AsyncSession, Depends(get_d
             detail="User not found",
         )
 
-    result = await db.execute(select(models.Post).where(models.Post.user_id == user_id))
+    result = await db.execute(
+        select(models.Post)
+        .where(models.Post.user_id == user_id)
+        .order_by(models.Post.date_posted.desc())
+    )
     posts = result.scalars().all()
     return posts
